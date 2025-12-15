b273dce038859e7642cb6c796cfc445d5b8d5abd
abhi88000 <abhishek@futurezminds.in>
2025-12-15 18:28:28 +0530

    Added Patient Dashboard for registration

---
+++ frontend/js/create-patient.js
@@ -0,0 +1,23 @@
+document.getElementById("patientForm").addEventListener("submit", async (e) => {
+  e.preventDefault();
+
+  const message = document.getElementById("message");
+  message.innerText = "";
+
+  const body = {
+    tenantId: document.getElementById("tenantId").value,
+    fullName: document.getElementById("fullName").value,
+    phone: document.getElementById("phone").value,
+    email: document.getElementById("email").value
+  };
+
+  try {
+    await apiPost("/patients", body);
+    message.innerText = "Patient created successfully";
+    message.style.color = "green";
+  } catch (err) {
+    message.innerText = "Patient already exists or invalid data";
+    message.style.color = "red";
+  }
+});
+
